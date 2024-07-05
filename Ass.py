320c83a8ba1f35a0795e5bd56dfdc27a410f0da9
omowumiii <159809488+omowumiii@users.noreply.github.com>
2024-07-05 08:46:54 +0100

    add

--- Ass.py
+++ Ass.py
@@ -19,13 +19,19 @@
 #         if amount > 0:
 #             self.account_balance += amount
 #             return f"Deposit has enter {self.name}'s account balance is {self.account_balance}"
+#     def withdraw(self,amount):
+#         if self.account_balance< amount:
+#             print("insufficient funds")
+#             return
+#         print(f"withdrawal of {amount} sucessful and your new balance is {self.account_balance}")
+#         self.account_balance-=amount
     
 #     def withdraw(self, amount):
 #         if amount>0:
 #             self.account_balance -= amount
 #             return f"Withdrawal has been sucessful {self.name}'s account balance is {self.account_balance}"
 
-#     def check_balance(self):
+    # def check_balance(self):
 #         return f"your current  account balance is {self.account_balance}"
         
 
@@ -34,7 +40,7 @@
 # wallet2 = BankAccount('Adekunle tobi', '231324423', 2000)
 
 # print(wallet1.deposit(500))
-# print(wallet1.withdraw(200))
+# print(wallet1.withdraw(22000))
 # print(wallet1.check_balance())
 # print(wallet2.deposit(500))
 # print(wallet2.withdraw(200))
@@ -47,34 +53,72 @@
 # Student
 # Lecturer
 
-class Person:
-   def __init__(self, name, age,address):
-        self.name = name
-        self.age = age
-        self.address=address
-   def person_info(self):
-         return(f"Hi there, this is {self.name}, and my {self.age},with {self.address}")
-class Student(Person):
-    def student_infor(self, course):
-        self.course= course
-        print (f" my name is {self.name}, and my age is  {self.age},  with address {self.address},and my course is {self.course}")
-class Lecturer(Person):
-    def lecturer_infor(self,departmrnt):
-        self.department=departmrnt
-def lecturer_info(self):
-       print (f"this is mr  {self.name},and his  {self.age} years old ,and his ID is department of {self.department}")
+# class Person:
+#    def __init__(self, name, age,address):
+#         self.name = name
+#         self.age = age
+#         self.address=address
+#    def person_info(self):
+#          return(f"Hi there, this is {self.name}, and my {self.age},with {self.address}")
+# class Student(Person):
+#     def student_infor(self, course):
+#         self.course= course
+#         print (f" my name is {self.name}, and my age is  {self.age},  with address {self.address},and my course is {self.course}")
+# class Lecturer(Person):
+#     def lecturer_infor(self,department):
+#         self.department=department
+#         print (f"this is mr  {self.name},and his  {self.age} years old ,and his is department of {self.department}")
 
-Persons=Person("tosin",34,"Ilorin")
-student =Student("Adekunle", 35, "lagos")
-lecturer =Lecturer("Dr. Ambali", 45, "Abuja")
+# Persons=Person("tobi",67,"aj")
+# student =Student("Adekunle", 35, "lagos")
+# lecturer =Lecturer("Dr. Ambali", 45, "Abuja")
 
-Persons=Person()
-student.student_infor("Accounting")
-lecturer.lecturer_infor("computer science")
+# Persons.person_info()
+# student.student_infor("Accounting")
+# lecturer.lecturer_infor("computer science")
 
+# question 3
+# create a Class name Parent and create two others
+# classes children and grandchildren. The children must 
+# inherit from their parents, and the grandchildren must inherit from both parents and children.
+# to create atleast two instances and methods for each class using python
 
 
 
         
+class Parent:
+    def __init__(self, family_name, eye_color):
+        self.family_name= family_name
+        self.eye_color=eye_color
+        
+    def display(self):
+        return f"my name is {self.family_name}, and the color of his eyes is  {self.eye_color}"
 
+class Children(Parent):
+    def __init__(self, family_name,eye_color,  height):
+        super().__init__(family_name, eye_color)
+        self.height=height
+    def display(self):
+        super().display()
+        return f"my family name is {self.family_name} and my eyes color is  {self.eye_color}, and my height is  {self.height} feet"
+
+
+
+class GrandChildren(Children):
+    def __init__(self, family_name,eye_color,  height, intelligence ):
+        super().__init__(family_name, eye_color,height)
+        self.intelligence = intelligence
+    def display(self):
+        super().display()
+        return f"my family name is {self.family_name}, and my eyes  color is  {self.eye_color}, my height is {self.height} feet, and my  intelligence is very {self.intelligence}"
+
+
+
+parent= Parent("Amabali","brown")
+print(parent.display())
+children= Children("Ambali","brown", 5.8)
+print(children.display())
+
+grandchildren= GrandChildren("Ambali","Brown",5.8,"High")
+print(grandchildren.display())
    
